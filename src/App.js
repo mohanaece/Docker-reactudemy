328d4789b033a0b62658336f51f3d068272de7ac
root <root@ip-172-31-11-127.ap-southeast-2.compute.internal>
2024-10-17 09:52:16 +0000

    merged and resolved the conflict in merge.txt

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changing on Branch feature
+          Changing on Branch feature -1
         </a>
       </header>
     </div>
